bf6f486b7c24b476c3ca883f8952107df0e7c135
yongvuthivann <vuthivann.yong007@gmail.com>
2024-04-15 21:49:36 +0700

    fix: header background color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import LangToggle from './lang-toggle';
 
 export default function Header() {
   return (
-    <div className="sticky top-0 flex items-center justify-between border-b-[0.5px] py-3">
+    <div className="sticky top-0 z-10 flex items-center justify-between border-b-[0.5px] bg-white py-3 dark:bg-[#020817]">
       <h1 className="font-bold">Nextjs App Boilerplate</h1>
       <LangToggle />
     </div>
