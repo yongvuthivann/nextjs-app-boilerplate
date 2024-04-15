2f8eeb6f0324f592809fe0a600963ba41e86675c
yongvuthivann <vuthivann.yong007@gmail.com>
2024-04-15 21:34:45 +0700

    fix: reorder toggle icon

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -17,16 +17,16 @@ export function ModeToggle() {
         <Sun className={`h-4 w-4`} />
       </div>
       <div
-        className={`cursor-pointer rounded-[100%] p-1 ${theme === 'dark' ? 'bg-slate-300 dark:bg-slate-500 ' : 'bg-transparent'}`}
-        onClick={() => setTheme('dark')}
-      >
-        <Moon className={`h-4 w-4`} />
-      </div>
-      <div
         className={`cursor-pointer rounded-[100%] p-1 ${theme === 'system' ? 'bg-slate-300 dark:bg-slate-500 ' : 'bg-transparent'}`}
         onClick={() => setTheme('system')}
       >
         <LaptopMinimal className={`h-4 w-4`} />
+      </div>
+      <div
+        className={`cursor-pointer rounded-[100%] p-1 ${theme === 'dark' ? 'bg-slate-300 dark:bg-slate-500 ' : 'bg-transparent'}`}
+        onClick={() => setTheme('dark')}
+      >
+        <Moon className={`h-4 w-4`} />
       </div>
     </div>
   );
