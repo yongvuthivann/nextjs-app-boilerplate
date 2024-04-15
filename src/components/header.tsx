efa563eae69e8ad85ce2a5b2440fab4a43549a8b
Yong Vuthivann <vuthivann.yong007@gmail.com>
2024-04-15 21:30:20 +0700

    fix: change toggle mode to footer (#7)
    * fix: remove mode toggle from header
    
    * fix: change toggle to footer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import { ModeToggle } from './mode-toggle';
 import LangToggle from './lang-toggle';
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-between py-3">
+    <div className="sticky top-0 flex items-center justify-between border-b-[0.5px] py-3">
       <h1 className="font-bold">Nextjs App Boilerplate</h1>
-      <div className="flex items-center space-x-4">
-        <LangToggle />
-        <ModeToggle />
-      </div>
+      <LangToggle />
     </div>
   );
 }
