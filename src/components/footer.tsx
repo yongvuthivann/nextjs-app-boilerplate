7eb518a9c22b9325233cee9c89464cdf3726b050
yongvuthivann <vuthivann.yong007@gmail.com>
2024-04-15 21:26:56 +0700

    fix: change toggle to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ModeToggle } from './mode-toggle';
 
 export default function Footer() {
   return (
     <footer className="mt-auto">
-      <div className="my-5 md:my-10">
+      <div className="my-5 flex items-center justify-between md:my-10">
         <p className="text-center text-xs">2024, Developed by Vuthivann Yong</p>
+        <ModeToggle />
       </div>
     </footer>
   );
