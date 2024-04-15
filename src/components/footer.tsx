3e42d1c9f669180da74c930ef9376e7b18ce19a6
yongvuthivann <vuthivann.yong007@gmail.com>
2024-04-15 21:56:24 +0700

    fix: add socail to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import { Github, Twitter } from 'lucide-react';
 import React from 'react';
 
 export default function Footer() {
@@ -5,6 +6,14 @@ export default function Footer() {
     <footer className="mt-auto">
       <div className="my-5 flex items-center justify-between md:my-10">
         <p className="text-center text-xs">2024, Developed by Vuthivann Yong</p>
+        <div className="flex items-center space-x-4">
+          <a href="https://github.com/yongvuthivann" target="__blank">
+            <Github className="h-5 w-5" />
+          </a>
+          <a href="https://twitter.com/vuthivann" target="__blank">
+            <Twitter className="h-5 w-5" />
+          </a>
+        </div>
       </div>
     </footer>
   );
