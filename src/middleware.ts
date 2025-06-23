a01e2077ed2c8e1597c031287819aec257be5638
Yong Vuthivann <vuthivann.yong007@gmail.com>
2025-06-24 00:42:51 +0700

    feat(*): upgraded next intl and lucide react version
    * feat(i18n): next-intl from v3 to v4
    
    * feat(footer): Current Year with Dayjs
    
    * chore: lucide react version

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
 
-export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'kh'],
-
-  // Used when no locale matches
-  defaultLocale: 'en',
-});
+export default createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(kh|en)/:path*'],
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
 };
