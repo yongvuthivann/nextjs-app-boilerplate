8cd71a8f81c8f711c766db4513119bd05e965e53
yongvuthivann <vuthivann.yong007@gmail.com>
2025-06-24 00:36:12 +0700

    feat(footer): Current Year with Dayjs

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,14 @@
+import dayjs from 'dayjs';
 import { Github, Twitter } from 'lucide-react';
-import React from 'react';
 
 export default function Footer() {
+  const currentYear = dayjs().format('YYYY');
   return (
     <footer className="mt-auto">
       <div className="my-5 flex items-center justify-between md:my-10">
-        <p className="text-center text-xs">2024, Developed by Vuthivann Yong</p>
+        <p className="text-center text-xs">
+          {currentYear}, Developed by Vuthivann Yong
+        </p>
         <div className="flex items-center space-x-4">
           <a href="https://github.com/yongvuthivann" target="__blank">
             <Github className="h-5 w-5" />
